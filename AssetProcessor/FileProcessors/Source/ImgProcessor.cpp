4288751f2718bf055826680e24513afe8989d96d
Jtljac <jtljac2@gmail.com>
2023-02-28 02:16:37 +0000

    Forced images to be 4 channels

--- AssetProcessor/FileProcessors/Source/ImgProcessor.cpp
+++ AssetProcessor/FileProcessors/Source/ImgProcessor.cpp
@@ -19,7 +19,7 @@ ImgProcessor::ImgProcessor() {
 
 void ImgProcessor::processFile(const std::string& src, const std::string& dest) {
     int width, height, channels;
-    unsigned char* data = stbi_load(src.data(), &width, &height, &channels, 0);
+    unsigned char* data = stbi_load(src.data(), &width, &height, &channels, 4);
 
     if (data == nullptr) {
         std::cout << "Failed to convert obj file: " << src << std::endl;
@@ -28,7 +28,7 @@ void ImgProcessor::processFile(const std::string& src, const std::string& dest) 
     }
 
     // channel-1 should map to the 8 bits per channel section
-    writeTexture(replaceExtension(dest, "ltex"), width, height, (TextureFormat) (channels - 1), data);
+    writeTexture(replaceExtension(dest, "ltex"), width, height, TextureFormat::R8G8B8A8, data);
 
     stbi_image_free(data);
 }
@@ -45,8 +45,11 @@ void ImgProcessor::writeTexture(const std::string& dest, uint32_t width, uint32_
         destFile.write(reinterpret_cast<char*>(&width), 4);
         // Height
         destFile.write(reinterpret_cast<char*>(&height), 4);
+
+        uint8_t formatInt = static_cast<uint32_t>(format);
+
         // Format
-        destFile.write(reinterpret_cast<char*>(&format), 1);
+        destFile.write(reinterpret_cast<char*>(&formatInt), 1);
     }
 
     // Write Body
